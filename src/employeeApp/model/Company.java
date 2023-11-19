2466bf12e3c955f72903278443b6f6cf94a842cf
mustafabasar96 <mustafabasar96@hotmail.com>
2023-11-19 15:36:42 +0300

    metodların bazıları düzenlendi

--- src/employeeApp/model/Company.java
+++ src/employeeApp/model/Company.java
@@ -1,4 +1,10 @@
 package employeeApp.model;
 
+import employeeApp.enums.Plan;
+
 public class Company {
+    private int id;
+    private String name;
+    private double giro;
+    private String[] developerNames;
 }
--- src/employeeApp/model/Employee.java
+++ src/employeeApp/model/Employee.java
@@ -1,5 +1,5 @@
 package employeeApp.model;
-
+import java.util.Arrays;
 import employeeApp.enums.Plan;
 
 public class Employee {
@@ -62,7 +62,45 @@ public class Employee {
         this.healthPlans = healthPlans;
     }
     public void addHealthPlan(int index, String name){
+        if(index < 0){
+            System.out.println("Index değeri 0'dan küçük olamaz.");
+        } else {
+            try {
+                boolean isExist = false;
+                for (String healthPlan : healthPlans) {
+                    if (healthPlan != null && healthPlan.equals(name)) {
+                        isExist = true;
+                        System.out.println(name + " dizi içerisinde mevcut.");
+                        break;
+                    }
+                }
 
-
+                if (!isExist) {
+                    if (healthPlans[index] == null) {
+                        healthPlans[index] = name;
+                    } else {
+                        System.out.println("İlgili index dolu.");
+                    }
+                }
+            } catch (ArrayIndexOutOfBoundsException ex){
+                System.out.println("Invalid index: " + index);
+            } catch (NullPointerException npe){
+                System.out.println("Null Pointer Exception: " + npe.getMessage());
+            } catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
     }
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", healthPlans=" + Arrays.toString(healthPlans) +
+                '}';
+    }
+
 }
+
+
