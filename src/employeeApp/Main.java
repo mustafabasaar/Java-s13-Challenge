4d36c3527b87d23ea1782086606481f3442e1208
mustafabasar96 <mustafabasar96@hotmail.com>
2023-11-20 13:14:08 +0300

    metodlar tamamlandı proje bitti

--- src/employeeApp/Main.java
+++ src/employeeApp/Main.java
@@ -1,5 +1,12 @@
 package employeeApp;
 
+import employeeApp.enums.Plan;
+import employeeApp.model.Company;
+import employeeApp.model.Employee;
+import employeeApp.model.Healthplan;
+
+import java.util.Arrays;
+
 /**
  * Uygulamanın amacı Java'da nesne tabanlı programlamanın temellerinin oluşturulmasıdır.
  *
@@ -12,7 +19,41 @@ public class Main
      */
     private static void workWithData ()
     {
+        Healthplan plan1 = new Healthplan(1, "A Sigorta", Plan.ADVANCE);
+        Healthplan plan2 = new Healthplan(2, "B Sigorta", Plan.MEDIUM);
 
+        System.out.println(plan1);
+        System.out.println(plan2);
+
+        String[] Healthplan = new String[3];
+        Healthplan[0] = plan1.getName();
+        Healthplan[1] = plan2.getName();
+
+        //["A Sigorta", "B Sigorta"]
+        Employee employee = new Employee(1, "Ali Veli",
+                "aliveli.com", "1234", Healthplan);
+
+        employee.addHealthPlan(0, "C Sigorta");
+        employee.addHealthPlan(2, "A Sigorta");
+        employee.addHealthPlan(2, "C Sigorta");
+        employee.addHealthPlan(3, "T Sigorta");
+        System.out.println(Arrays.toString(employee.getHealthPlans()));
+
+        System.out.println("**********************************");
+        Company company = new Company(1, "Workintech",
+                1000000000, new String[]{"Dogancan Kınık", "Omer Fatih Tanriverdi", null});
+
+        company.addEmployee(0, "Ali Veli");
+        company.addEmployee(2, "Mehmet Ali");
+        company.addEmployee(-1, "Mehmet Ali");
+        company.addEmployee(5, "Mehmet Ali");
+
+        System.out.println(Arrays.toString(company.getDeveloperNames()));
+        System.out.println(company);
+
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        //double num = 1000000000;
     }
 
     /**
--- src/employeeApp/model/Company.java
+++ src/employeeApp/model/Company.java
@@ -2,9 +2,81 @@ package employeeApp.model;
 
 import employeeApp.enums.Plan;
 
+import java.util.Arrays;
+
 public class Company {
     private int id;
     private String name;
     private double giro;
     private String[] developerNames;
+
+
+    private void checkGiro(double giro){
+        if(giro < 0){
+            this.giro = 0;
+        } else{
+            this.giro = giro;
+        }
+    }
+
+    public Company(int id, String name, double giro, String[] developerNames) {
+        this.id = id;
+        this.name = name;
+        checkGiro(giro);
+        this.developerNames = developerNames;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGiro() {
+        return giro;
+    }
+
+    public void setGiro(double giro) {
+        checkGiro(giro);
+    }
+
+    public String[] getDeveloperNames() {
+        return developerNames;
+    }
+
+    public void setDeveloperNames(String[] developerNames) {
+        this.developerNames = developerNames;
+    }
+
+    public void addEmployee(int index, String name){
+        try {
+            if (developerNames[index] == null) {
+                developerNames[index] = name;
+            } else {
+                System.out.println("İlgili index dolu.");
+            }
+        }catch (ArrayIndexOutOfBoundsException ex){
+            System.out.println("Olmayan idndex için atama yapıldı: " + index);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", giro=" + giro +
+                ", developerNames=" + Arrays.toString(developerNames) +
+                '}';
+    }
 }
